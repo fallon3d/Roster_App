952c8939eef022009d756bfee49f5643fe6093ca
fallon3d <fallon3d@gmail.com>
2025-08-23 15:38:35 -0400

    Create __init__.py

---
+++ rotation_core/rotation_core/__init__.py
@@ -0,0 +1,15 @@
+# FILE: rotation_core/__init__.py
+"""
+rotation_core package: domain models, fairness, suitability, solver, IO, and validation.
+"""
+__all__ = [
+    "models",
+    "aliases",
+    "fairness",
+    "suitability",
+    "hungarian",
+    "assignment",
+    "validation",
+    "io",
+    "explain",
+]
